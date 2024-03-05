0787c00b4cc558475e3696b7ad86a3a92c3ec88a
aayushiTatva <156199863+aayushiTatva@users.noreply.github.com>
2024-03-05 12:27:17 +0530

    Add files via upload

---
+++ HTML Training Assignment/js/Custom.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+$("#files").change(function () {
+    console.log("function");
+    filename = this.files[0].name;
+    $("#choosenfile").text(filename);
+});
+
+
+function openCity(evt, cityName) {
+    evt.preventDefault();
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
